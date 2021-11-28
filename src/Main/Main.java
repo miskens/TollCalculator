b0f1975f42e044fb212f65712c8ef0a024cdea69
misken <miskens@hotmail.com>
2021-11-28 16:59:57 +0100

    Added inheritance of Vehicle to Car and Motorcycle.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,17 @@
 package Main;
 
+import Vehicle.Car;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        TollCalculator tc = new TollCalculator();
+        Car car = new Car("123 abc");
+        LocalDateTime cameraFlashTime = LocalDateTime.of(2021,11,30,8,31,00);
+    
+        int fee = tc.getTollFee(car, cameraFlashTime);
 
-        New TollCalculator 
+        System.out.println(fee); // prints out the fee when we are done calculating
      
     }
 
