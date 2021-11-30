f6b82dadd2f284783e6710f60f39aaed98171b27
Mikael Puusaari <miskens@hotmail.com>
2021-11-30 17:33:47 +0100

    Added day incremention to Mockdata. Added Check for camera flash for the last hour.

--- src/Main/TollCalculator.java
+++ src/Main/TollCalculator.java
@@ -2,29 +2,51 @@ package Main;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import Vehicle.Vehicle;
 
 public class TollCalculator {
 
-    public int getTollFee(Vehicle currentVehicle, int currentFee, LocalDateTime timeOfCurrentFlash) {
+    public int getTollFee(Vehicle currentVehicle, int lastFee, int currentFee, LocalDateTime timeOfCurrentFlash) {
         String vehicleType = currentVehicle.getClass().getSimpleName();
+        int fee;
         int hour = timeOfCurrentFlash.getHour();
+        LocalDateTime timeOfLastCameraFlash;
         DayOfWeek day = timeOfCurrentFlash.getDayOfWeek();
 
         if(currentFee >= 60 || day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
             return 0;
         }
 
-        int fee = getFeeDependingOnHoursAndVehicleType(hour, vehicleType);
+        fee = getFeeDependingOnHoursAndVehicleType(hour, vehicleType);
+
+        if (currentVehicle.getTimeOfLastCameraFlash() != null) {
+            timeOfLastCameraFlash = currentVehicle.getTimeOfLastCameraFlash();
+            long minutesSinceLastFlash = timeOfLastCameraFlash.until(timeOfCurrentFlash, ChronoUnit.MINUTES);
+
+            if(minutesSinceLastFlash <= 60) {
+                if(lastFee > fee) {
+                    return lastFee - fee;
+                }
+                else if (fee > lastFee) {
+                    currentVehicle.setLastFee(fee);
+                    return fee - lastFee;
+                }
+                else {
+                    return 0;
+                }
+            }
+        }
 
         if (currentFee + fee >= 60) {
             currentVehicle.setCurrentFee(60);
             return 60 - currentFee;
         }
 
-
         currentVehicle.setCurrentFee(currentFee + fee);
+        currentVehicle.setLastFee(fee);
+        currentVehicle.setTimeOfLastCameraFlash(timeOfCurrentFlash);
         return fee;
     }
 
