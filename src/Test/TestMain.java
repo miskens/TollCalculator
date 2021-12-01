327a15664506a4f34e7a99c61e50087baab4a928
Molnet2013 <winlof@hotmail.com>
2021-12-01 15:02:49 +0100

    Started with first two tests, fee and vehicle type

---
+++ src/Test/TestMain.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        TollCalculatorTest tct = new TollCalculatorTest();
+
+        tct.testFeesVariesBetween8and18();
+    }
+
+}
