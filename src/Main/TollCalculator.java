c44c26da3fd5d1966bb576f2dae41fed2cb8c977
Mikael Puusaari <miskens@hotmail.com>
2021-12-03 19:04:17 +0100

    Added/Fixed reset of total toll fee when new day(first flash that is new day).

--- src/Main/TollCalculator.java
+++ src/Main/TollCalculator.java
@@ -7,12 +7,16 @@ import Vehicle.Vehicle;
 
 public class TollCalculator {
 
-    public int getTollFee(Vehicle currentVehicle, int lastFee, int currentTotalFee, LocalDateTime timeOfCurrentFlash) {
+    public int getTollFee(Vehicle[] vehicles, Vehicle currentVehicle, int lastFee, int currentTotalFee, LocalDateTime timeOfCurrentFlash) {
         String vehicleType = currentVehicle.getClass().getSimpleName();
         int fee;
         int hour = timeOfCurrentFlash.getHour();
-        LocalDateTime timeOfLastCameraFlash;
+        LocalDateTime timeOfLastCameraFlash = currentVehicle.getTimeOfLastCameraFlash();
         DayOfWeek day = timeOfCurrentFlash.getDayOfWeek();
+
+        if (timeOfLastCameraFlash != null) {
+            resetcurrentTotalFeeIfNewday(vehicles, timeOfCurrentFlash.getDayOfMonth(), timeOfLastCameraFlash.getDayOfMonth(), currentVehicle);
+        }
 
         if (currentTotalFee >= 60 || day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
             return 0;
@@ -20,8 +24,7 @@ public class TollCalculator {
 
         fee = getFeeDependingOnHoursAndVehicleType(hour, vehicleType);
 
-        if (currentVehicle.getTimeOfLastCameraFlash() != null) {
-            timeOfLastCameraFlash = currentVehicle.getTimeOfLastCameraFlash();
+        if (currentVehicle.getTimeOfLastCameraFlash() != null) { 
             long minutesSinceLastFlash = timeOfLastCameraFlash.until(timeOfCurrentFlash, ChronoUnit.MINUTES);
 
             if (minutesSinceLastFlash <= 60) {
@@ -45,6 +48,19 @@ public class TollCalculator {
         currentVehicle.setLastFee(fee);
         currentVehicle.setTimeOfLastCameraFlash(timeOfCurrentFlash);
         return fee;
+    }
+
+    private void resetcurrentTotalFeeIfNewday(Vehicle[] vehicles, int dayOfMonth, int dayOfMonth2, Vehicle currentVehicle) {
+        // If day of this flash > day of last flash:
+        if (dayOfMonth > dayOfMonth2) {
+            //      Code to send currentTotal for invoicing or whatever since fees shouldn´t be transferred to next day
+            //      Makes sure the calculation for max 60kr/day can be done using dayOfMonth above
+
+            //      set currentTotal to 0
+            
+            for (Vehicle vehicle : vehicles)
+            vehicle.setCurrentTotalFee(0);
+            }
     }
 
     private int getFeeDependingOnHoursAndVehicleType(int hour, String vehicleType) {
