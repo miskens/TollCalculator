04b8ffaaf8e9a39598928fef82df1e1143fbc4db
Mikael Puusaari <miskens@hotmail.com>
2021-12-04 15:59:50 +0100

    Created package: Interfaces. Moved ITollable there.

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,5 +1,7 @@
 package Vehicle;
 
+import Interfaces.ITollable;
+
 public class Car extends Vehicle implements ITollable {
     public Car(String regNr) {
         super(regNr);
--- src/Vehicle/Motorcycle.java
+++ src/Vehicle/Motorcycle.java
@@ -1,5 +1,7 @@
 package Vehicle;
 
+import Interfaces.ITollable;
+
 public class Motorcycle extends Vehicle implements ITollable {
     public Motorcycle(String regNr) {
         super(regNr);
